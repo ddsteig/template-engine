8cac06137ea891af7e7e2fa7f9c05e8c0f18a5e3
Danny <tekhexrax@yahoo.com>
2020-08-24 21:02:46 -0400

    completed app.js code, added all classes working

--- Develop/app.js
+++ Develop/app.js
@@ -10,9 +10,154 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+let employees = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+// inquirer will prompt manager information first,
+// then user can add more employees that are engineers or interns
+
+function promptUser() {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter Manager Name:",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "Enter Manager Email:",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "Enter Manager ID:",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "Enter Office Number:",
+        name: "officeNumber",
+      },
+    ])
+    .then(function (info) {
+      const manager = new Manager(
+        info.name,
+        info.email,
+        info.id,
+        info.officeNumber
+      );
+      employees.push(manager);
+    })
+    .then(function addEmployee() {
+      return inquirer
+        .prompt([
+          {
+            type: "list",
+            message: "Select Employee Role:",
+            name: "role",
+            choices: ["Engineer", "Intern"],
+          },
+        ])
+        .then(function (input) {
+          switch (input.role) {
+            case "Engineer":
+              return inquirer
+                .prompt([
+                  {
+                    type: "input",
+                    message: "Enter Employee Name:",
+                    name: "name",
+                  },
+                  {
+                    type: "input",
+                    message: "Enter Employee Email:",
+                    name: "email",
+                  },
+                  {
+                    type: "input",
+                    message: "Enter Employee ID:",
+                    name: "id",
+                  },
+                  {
+                    type: "input",
+                    message: "Enter Github:",
+                    name: "github",
+                  },
+                  {
+                    type: "list",
+                    message: "Add another team member?",
+                    name: "answer",
+                    choices: ["Yes", "No"],
+                  },
+                ])
+                .then(function (info) {
+                  const engineer = new Engineer(
+                    info.name,
+                    info.email,
+                    info.id,
+                    info.github
+                  );
+                  employees.push(engineer);
+
+                  switch (info.answer) {
+                    case "Yes":
+                      return addEmployee();
+                    case "No":
+                      return;
+                  }
+                });
+            case "Intern":
+              return inquirer
+                .prompt([
+                  {
+                    type: "input",
+                    message: "Enter Employee Name:",
+                    name: "name",
+                  },
+                  {
+                    type: "input",
+                    message: "Enter Employee Email:",
+                    name: "email",
+                  },
+                  {
+                    type: "input",
+                    message: "Enter Employee ID:",
+                    name: "id",
+                  },
+                  {
+                    type: "input",
+                    message: "Enter School:",
+                    name: "school",
+                  },
+                  {
+                    type: "list",
+                    message: "Add another team member?",
+                    name: "answer",
+                    choices: ["Yes", "No"],
+                  },
+                ])
+                .then(function (info) {
+                  const intern = new Intern(
+                    info.name,
+                    info.email,
+                    info.id,
+                    info.school
+                  );
+                  employees.push(intern);
+
+                  switch (info.answer) {
+                    case "Yes":
+                      return addEmployee();
+                    case "No":
+                      return;
+                  }
+                });
+          }
+        });
+    });
+}
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -23,6 +168,14 @@ const render = require("./lib/htmlRenderer");
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
+
+promptUser().then(function () {
+  let html = render(employees);
+  fs.writeFile(outputPath, html, (err) => {
+    if (err) throw err;
+    console.log("The file has been saved!");
+  });
+});
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
